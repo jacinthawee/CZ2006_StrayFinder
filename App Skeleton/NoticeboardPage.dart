dac1d1ca704da6053d1fe16789de78bc19a3928e
Jerry-WoL <57562093+Jerry-WoL@users.noreply.github.com>
2022-03-29 16:36:50 +0800

    Add files via upload

---
+++ App Skeleton/NoticeboardPage.dart
@@ -0,0 +1,18 @@
+/// The class that represents the Noticeboard page 
+/// * Author: Hu Zhuangyu
+/// * Version: 1.0
+/// * Since: 2022-03-29
+class InjuredCatCard {
+	
+
+    /// A method to build the page
+	build() {
+	
+	}
+
+
+    /// A method to remove an injured cat from the Noticeboard
+    /// [cat_to_remove]: the cat to be removed
+	_removeInjured(StrayCat cat_to_remove) {
+	
+	}
