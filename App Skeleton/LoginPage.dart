e2cea9ad2f6d2ca8e8f9dda2c9797980a65d9f4c
shivangi-702 <85705269+shivangi-702@users.noreply.github.com>
2022-03-29 16:06:33 +0800

    Add files via upload

---
+++ App Skeleton/LoginPage.dart
@@ -0,0 +1,39 @@
+/**
+* Represents the class that controls the methods of the login page
+*/
+class LoginPage{
+    /** a private attribute that allows the passowrd enetered during login to be viewed as '****' if true,
+    or to be viewed as the actual string if false
+    */ 
+    boolean _isHidden;
+
+    /** 
+    * 
+    */
+    build(){}
+
+    /** 
+    * Method for returning to the login page for the volunteers 
+    */
+    goHome(){}
+
+    /** 
+    * Method that allows the volunteer to create an account on the app using their organizations name
+    */
+    goRegister(){}
+
+    /** 
+    * Method that allows voulnteers to view all the tabs 
+    */
+    goVolunteerTabs(){}
+
+    /** 
+    * Method to toggle between hiding and showing the password 
+    */
+    _togglePasswaordView(){}
+
+    /** 
+    * Method for changing the password of the volunteers account  
+    */
+    resetPassword(){}
+}
