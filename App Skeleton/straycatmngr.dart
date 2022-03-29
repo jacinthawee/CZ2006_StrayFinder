f5283a836669b5ffcbb833d95bf008228067de07
Jacintha Wee <78328036+jacinthawee@users.noreply.github.com>
2022-03-29 15:40:14 +0800

    Add files via upload

---
+++ App Skeleton/straycatmngr.dart
@@ -0,0 +1,17 @@
+/// The control class for StrayCat.
+class StrayCatMngr{
+    /// THe list of all StrayCats exisiting in the database.
+    Array[StrayCat] strayCatList;
+    StrayCatMngr({required this.strayCatList});
+
+    /// Returns all last seen locations of the StrayCats in the database.
+    getAllCatLocation(){}
+    /// ??
+    getCatInfo(){}
+    /// Returns the unique ID of thhe corresponding stray cat, which is injured.
+    getInjuredCatID(){}
+    /// Returns the location of a specific stray cat to be queried.
+    getSpecificCatLocation(){}
+    /// When a volunteer decides to handle a case of an injured stray cat from the Injury Noticeboard, the injury status of the stray cat is changed to False and stray cat info is removed ftom the Noticeboard.
+    manageInjury(){}
+}
