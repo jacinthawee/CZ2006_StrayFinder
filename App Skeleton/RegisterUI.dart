041cb447120acdac364d7206dff7f511e7731dd6
shivangi-702 <85705269+shivangi-702@users.noreply.github.com>
2022-03-29 16:09:55 +0800

    Add files via upload

---
+++ App Skeleton/RegisterUI.dart
@@ -0,0 +1,12 @@
+///Represents the user interface that the voluneer interacts with during registration 
+class RegisterUI{
+    ///A boolean attribute that allows the passowrd enetered during login to be viewed as '****' if true,
+    ///or to be viewed as the actual string if false
+    Boolean _isHiddenPassword;
+
+    ///Method that takes the user to the login
+    void goLogin(BuildContext context){}
+
+    ///Method that saves the volunteers registration detail onto the database
+    registerUserToDatabase(){}
+}
---
+++ App Skeleton/RegistrationMngr.dart
@@ -0,0 +1,9 @@
+///Represents the class that controls the methods of the registration page
+class RegistrationMngr{
+    ///Boolean method that checks if the organization name already exists in the
+    ///the database during registration 
+    Boolean checkDupOrg(){}
+
+    ///Method that allows the user to create an account after all the information for 
+    createAccount(){}
+}
