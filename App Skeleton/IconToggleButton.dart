dfdcf497afc8f377dc4311ffe268442843083845
valencialie11 <59992568+valencialie11@users.noreply.github.com>
2022-03-29 15:41:07 +0800

    Add files via upload

---
+++ App Skeleton/IconToggleButton.dart
@@ -0,0 +1,15 @@
+
+///A class that represents a widget of a button that we can toggle between checked and unchecked
+class IconToggleButton{
+
+  ///An attribute that returns either true or false when the button is pressed
+  late bool isSelected;
+
+  ///An attribute such that we are able to implement the onpressed function of the button
+  late Function onPressed;
+
+  ///A method to build the page
+  build(){}
+
+
+}
---
+++ App Skeleton/ThankYouForReportingPage.dart
@@ -0,0 +1,6 @@
+/// A class that represents the page that thanks the user for reporting a stray cat
+class ThankYouForReportingPage{
+
+  /// A method to build the page
+  build(){}
+}
