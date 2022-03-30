8b83c3b7c47c88351ca19aa43973ee19124629cd
Jacintha Wee <78328036+jacinthawee@users.noreply.github.com>
2022-03-30 10:16:28 +0800

    Update straycat.dart

--- App Skeleton/straycat.dart
+++ App Skeleton/straycat.dart
@@ -14,28 +14,11 @@ class StrayCat{
   Injury injury;
   /// The injurt status of the stray cat, either True or False.
   bool statusInjured;
+  /// The latitude coordinate of the last seen position of the stray cat.
+  double lastSeenLatitude;
+  /// The longitude coordinate of the last seen position of the stray cat.
+  double lastSeenLongitude;
   /// The contact number of the passerby if he/she chooses to send the stray cat to the vet (if the cat is injured).
   String contact;
-//   String condition;
-  StrayCat({required this.id, required this.name, required this.breed, required this.lastSeenLocation, required this.pictureCat, required this.injury, required this.statusInjured, required this.contact, /*required this.condition*/});
-
-  /// Returns the name of the stray cat.
-  getName(){}
-  /// Returns the breed of the stray cat.
-  getBreed(){}
-  /// Returns the last updated location of the stray cat based on GPS location of passerby which reported the stray cat.
-  getLocation(){}
-  /// Returns the saved picture of the stray cat.
-  getPictureCat(){}
-  /// Returns the picture of the injury of the tray cat if the passerby reports it to be injured.
-  getPictureInjury(){}
-  /// Returns the injury status of the stray cat, either True or False.
-  getStatus(){}
-  /// Assign a name to the stray cat if there are no records of it in the app database.
-  setName(){}
-  /// Assign the breed of the stray cat as predicted using the breed predictor API.
-  setBreed(){}
-  ///Assign the injury status of the stray cat, either True or  False.
-  setStatus(){}
-
+  StrayCat({required this.id, required this.name, required this.breed, required this.lastSeenLocation, required this.pictureCat, required this.injury, required this.statusInjured, required this.lastSeenLatitude, required this.lastSeenLongitude, required this.contact});
 }
