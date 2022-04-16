df173c400368d549de642760db859264a3e1e56e
XYChen21 <coco.001121@gmail.com>
2022-04-16 21:18:25 +0800

    Update AdoptManager.dart

--- stray_finder/lib/managers/AdoptManager.dart
+++ stray_finder/lib/managers/AdoptManager.dart
@@ -3,7 +3,7 @@ import '../classes/AdoptOrg.dart';
 class AdoptMngr{
 
 
-  static List<AdoptOrg> adoptionCentres = [AdoptOrg(id: 1, name: "Animal Lovers League", email: "general@animalloversleague.com", contact: "9670 8052", address: "The Animal Lodge, 59 Sungei Tengah Road, Block Q #01-29, Singapore 699014", openinghours: "By Appointment Only", fee: "Free", website: "https://www.animalloversleague.com/", pic: 'images/all.jpeg',
+  static List<AdoptOrg> _adoptionCentres = [AdoptOrg(id: 1, name: "Animal Lovers League", email: "general@animalloversleague.com", contact: "9670 8052", address: "The Animal Lodge, 59 Sungei Tengah Road, Block Q #01-29, Singapore 699014", openinghours: "By Appointment Only", fee: "Free", website: "https://www.animalloversleague.com/", pic: 'images/all.jpeg',
       pic2: 'images/all2.jpeg', pic3: 'images/all3.jpeg'),
     AdoptOrg(id: 2, name: "Cat Welfare Society ", email: "info@catwelfare.org", contact: "NA", address: "NA", openinghours: "NA", fee: "40-80 SGD", website: "https://www.catwelfare.org/", pic:'images/cws.jpeg', pic2:'images/cws2.jpeg', pic3:'images/cws3.jpeg'),
     AdoptOrg(id: 3, name: "Causes for Animals Singapore ", email: "adoption@causesforanimals.com", contact: "9793 7162 / 9697 3491", address: "NA", openinghours: "NA", fee: "100-150 SGD", website: "https://www.causesforanimals.com/", pic: 'images/cas.jpeg', pic2: 'images/cas2.jpeg', pic3: 'images/cas3.jpeg'),
@@ -14,4 +14,7 @@ class AdoptMngr{
     AdoptOrg(id: 8, name: "SPCA", email: "enquiries@spca.org.sg", contact: "6287 5355", address: "50 Sungei Tengah Road, Singapore 699012", openinghours: "By Appointment Only", fee: "25-200 SGD", website: "https://www.spca.org.sg/", pic: 'images/spca.jpeg', pic2: 'images/spca2.jpeg', pic3: 'images/spca3.jpeg'),
     AdoptOrg(id: 9, name: "The Cat Museum / Kitten Sanctuary", email: "purr@kittensanctuarysg.org", contact: "NA", address: "737A North Bridge Road, Singapore 198705", openinghours: "By Appointment Only", fee: "80-150 SGD", website: "https://www.kittensanctuarysg.org/", pic: 'images/ks.jpeg', pic2: 'images/ks2.jpeg', pic3: 'images/ks3.jpeg'),
   ];
+  static List<AdoptOrg> getAdoptionCentres(){
+    return _adoptionCentres;
+  }
 }
