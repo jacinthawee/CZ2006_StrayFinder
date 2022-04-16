3769f5bf18ccf2203b9bfdd84bf78a9a5cca3dd8
Xingyu Chen <coco.001121@gmail.com>
2022-04-16 21:11:16 +0800

    added manager classes

---
+++ stray_finder/lib/managers/AdoptManager.dart
@@ -0,0 +1,17 @@
+import '../classes/AdoptOrg.dart';
+
+class AdoptMngr{
+
+
+  var adoptionCentres = [AdoptOrg(id: 1, name: "Animal Lovers League", email: "general@animalloversleague.com", contact: "9670 8052", address: "The Animal Lodge, 59 Sungei Tengah Road, Block Q #01-29, Singapore 699014", openinghours: "By Appointment Only", fee: "Free", website: "https://www.animalloversleague.com/", pic: 'images/all.jpeg',
+      pic2: 'images/all2.jpeg', pic3: 'images/all3.jpeg'),
+    AdoptOrg(id: 2, name: "Cat Welfare Society ", email: "info@catwelfare.org", contact: "NA", address: "NA", openinghours: "NA", fee: "40-80 SGD", website: "https://www.catwelfare.org/", pic:'images/cws.jpeg', pic2:'images/cws2.jpeg', pic3:'images/cws3.jpeg'),
+    AdoptOrg(id: 3, name: "Causes for Animals Singapore ", email: "adoption@causesforanimals.com", contact: "9793 7162 / 9697 3491", address: "NA", openinghours: "NA", fee: "100-150 SGD", website: "https://www.causesforanimals.com/", pic: 'images/cas.jpeg', pic2: 'images/cas2.jpeg', pic3: 'images/cas3.jpeg'),
+    AdoptOrg(id: 4, name: "Love Kuching Project", email: "assist@lovekuchingproject.org", contact: "NA", address: "NA", openinghours: "NA", fee: "65 SGD", website: "https://www.facebook.com/luvkuching/", pic: 'images/lkp.jpeg', pic2: 'images/lkp2.jpeg', pic3: 'images/lkp3.jpeg'),
+    AdoptOrg(id: 5, name: "Mutts & Mittens", email: "info@muttsnmittens.com", contact: "6583 7371 / 6583 7372", address: "Lucky Cat Inn, 175 Guillemard Road, Singapore 399721", openinghours: "NA", fee: "250 SGD", website: "https://www.muttsnmittens.com/", pic: 'images/mandm.jpeg', pic2: 'images/mandm2.jpeg', pic3: 'images/mandm3.jpeg')
+    ,AdoptOrg(id: 6, name: "Project Save Our Street Kittens", email: "NA", contact: "NA", address: "NA", openinghours: "NA", fee: "Free", website: "https://www.facebook.com/pages/category/Community-Organization/Project-SOKS-Save-Our-Street-Kittens-Singapore-174014033246553/", pic: 'images/psoks.jpeg', pic2: 'images/psoks2.jpeg', pic3: 'images/psoks3.jpeg'),
+    AdoptOrg(id: 7, name: "Purely Adoptions", email: "hello@purelyadoptions.com", contact: "9001 8848", address: "NA", openinghours: "NA", fee: "100-250 SGD", website: "https://www.purelyadoptions.com/", pic: 'images/pa.jpeg', pic2: 'images/pa2.jpeg', pic3: 'images/pa3.jpeg'),
+    AdoptOrg(id: 8, name: "SPCA", email: "enquiries@spca.org.sg", contact: "6287 5355", address: "50 Sungei Tengah Road, Singapore 699012", openinghours: "By Appointment Only", fee: "25-200 SGD", website: "https://www.spca.org.sg/", pic: 'images/spca.jpeg', pic2: 'images/spca2.jpeg', pic3: 'images/spca3.jpeg'),
+    AdoptOrg(id: 9, name: "The Cat Museum / Kitten Sanctuary", email: "purr@kittensanctuarysg.org", contact: "NA", address: "737A North Bridge Road, Singapore 198705", openinghours: "By Appointment Only", fee: "80-150 SGD", website: "https://www.kittensanctuarysg.org/", pic: 'images/ks.jpeg', pic2: 'images/ks2.jpeg', pic3: 'images/ks3.jpeg'),
+  ];
+}
---
+++ stray_finder/lib/managers/UserVolunteerMngr.dart
@@ -0,0 +1,85 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class UserVolunteerMngr {
+
+  static Future<void> addUser(String email, String contactNo, String orgName, String address) async{
+    await FirebaseFirestore.instance.collection('users')
+        .doc(email)
+        .set({
+      'user_email': email,
+      'user_contact': contactNo,
+      'org_name': orgName,
+      'org_address': address,
+    })
+        .then((value) => print("Volunteer Added"))
+        .catchError((error) => print("Failed to add volunteer: $error"));
+  }
+
+  static Future<Map<String, dynamic>> getVolunteerInfo(String email) async {
+    Map<String, dynamic> user = {};
+    await FirebaseFirestore.instance
+        .collection('users')
+        .where('user_email', isEqualTo: email)
+        .get()
+        .then((value) {
+      value.docs.forEach((result) {
+        // user.update('user_email', (value) => result.data()['user_email']);
+        // user.update('user_contact', (value) => result.data()['user_contact']);
+        // user.update('org_name', (value) => result.data()['org_name']);
+        // user.update('org_address', (value) => result.data()['org_address']);
+        user['user_email'] = email;
+        user['user_contact'] = result.data()['user_contact'];
+        user['org_name'] = result.data()['org_name'];
+        user['org_address'] = result.data()['org_address'];
+      });
+    });
+    return user;
+  }
+  static Future<bool> checkDupName(String name) async{
+    var result = await FirebaseFirestore.instance
+        .collection('users')
+        .where('org_name', isEqualTo: name)
+        .get();
+    if (result.docs.isEmpty) {
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
+  static Future<void> editProfile(String email, String newName, String newContact, String newAddress) async{
+    await FirebaseFirestore.instance
+        .collection('users')
+        .doc(email)
+        .update({'org_name': newName, 'user_contact': newContact, 'org_address': newAddress})
+        .then((value) => print("Profile updated"))
+        .catchError((error) => print("Failed to update profile: $error"));
+  }
+
+  // Future<void> editOrgName(String email, String newName) async{
+  //   return await FirebaseFirestore.instance
+  //       .collection('users')
+  //       .doc(email)
+  //       .update({'org_name': newName})
+  //       .then((value) => print("Organisation name updated"))
+  //       .catchError((error) => print("Failed to update organisation name: $error"));
+  // }
+  //
+  // Future<void> editContactNo(String email, String newContact) async{
+  //   return await FirebaseFirestore.instance
+  //       .collection('users')
+  //       .doc(email)
+  //       .update({'user_contact': newContact})
+  //       .then((value) => print("Volunteer contact no updated"))
+  //       .catchError((error) => print("Failed to update volunteer contact no: $error"));
+  // }
+  //
+  // Future<void> editAddress(String email, String newAddress) async{
+  //   return await FirebaseFirestore.instance
+  //       .collection('users')
+  //       .doc(email)
+  //       .update({'org_address': newAddress})
+  //       .then((value) => print("Volunteer address updated"))
+  //       .catchError((error) => print("Failed to update volunteer address: $error"));
+  // }
+}
